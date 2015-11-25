1ef389a6c38c0c8811288c22368f0180070f2cd2
stevenjmarsh <sjm.atl@gmail.com>
2015-11-25 16:32:13 -0500

    include contact info in order form

--- client/templates/public/order.js
+++ client/templates/public/order.js
@@ -12,6 +12,45 @@ Template.order.onCreated(function () {
   this.currentOrder.set("pizza", {"name": "Pick a pizza!", "price": 0});
 });
 
+Template.order.helpers({
+  customer: function () {
+    if (Meteor.user()) {
+      var getCustomer = Customers.findOne({"userId": Meteor.userId()});
+    } else {
+      var getCustomer = {};
+    }
+
+    if (getCustomer) {
+      getCustomer.context = "order";
+      return getCustomer;
+    }
+  },
+
+  order: function () {
+    var currentOrder = Template.instance().currentOrder,
+      type = currentOrder.get('type'),
+      pizza = currentOrder.get('pizza'),
+      price = currentOrder.get('price');
+
+    if (type !== 'Custom Pizza') {
+      var getPizza = pizza._id ? Pizza.findOne({"_id": pizza._id}) : pizza;
+    } else {
+      var getPizza = {
+        name: 'Build your custom pizza up above!',
+        price: 1000
+      };
+    }
+
+    if (getPizza) {
+      return {
+        type: pizza.name !== 'Pick a pizza!' ? type : null,
+        pizza: getPizza,
+        price: getPizza.price
+      }
+    }
+  }
+});
+
 Template.order.events({
     'click .nav-tabs li': function(event, template) {
       var orderType = $(event.target).closest("li").data("pizza-type");
