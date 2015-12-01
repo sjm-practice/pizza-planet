b3137f3d7c464a2b29b2357cf7a07ffa2a739f19
stevenjmarsh <sjm.atl@gmail.com>
2015-12-01 12:17:39 -0500

    in progress: submit order

--- client/templates/public/order.js
+++ client/templates/public/order.js
@@ -12,6 +12,119 @@ Template.order.onCreated(function () {
   this.currentOrder.set("pizza", {"name": "Pick a pizza!", "price": 0});
 });
 
+Template.order.onRendered(function () {
+
+  var thisTemplate = Template.instance();
+
+  $('#place-order').validate({
+    rules: {
+      customPizzaName: {
+        required: true
+      },
+      name: {
+        required: true
+      },
+      telephone: {
+        required: true
+      },
+      streetAddress: {
+        required: true
+      },
+      secondaryAddress: {
+        required: true
+      },
+      city: {
+        required: true
+      },
+      state: {
+        required: true
+      },
+      zipCode: {
+        required: true
+      },
+      emailAddress: {
+        required: true,
+        email: true
+      },
+      password: {
+        required: true,
+        minlength: 6
+      }
+    },
+    submitHandler: function () {
+      var orderData = thisTemplate.currentOrder,
+        type = orderData.get('type'),
+        pizza = orderData.get('pizza'),
+        order = {};
+
+      if (Meteor.user()) {
+        order.customer = Meteor.userId();
+      } else {
+        order.customer = {
+          name:             template.find("[name]='name']").value,
+          telephone:        template.find("[name]='telephone']").value,
+          streetAddress:    template.find("[name]='streetAddress']").value,
+          secondaryAddress: template.find("[name]='secondaryAddress']").value,
+          city:             template.find("[name]='city']").value,
+          state:            template.find("[name]='state']").value,
+          zipCode:          template.find("[name]='zipCode']").value
+        };
+
+        order.credentials = {
+          email: template.find("[name]='emailAddress']").value,
+          password: template.find("[name]='password']").value
+        };
+      }
+
+      if (type === "Custom Pizza") {
+        var meatToppings = [],
+            nonMeatToppings = [];
+
+        $("[name='meatTopping']:checked").each(function (index, element) {
+          meatToppings.push(element.value);
+        });
+
+        $("[name='nonMeatTopping']:checked").each(function (index, element) {
+          nonMeatToppings.push(element.value);
+        });
+
+        var customPizza = {
+          name: template.find("[name='customPizzaName']").value,
+          size: template.find("[name='size'] option:selected").value,
+          crust: template.find("[name='crust'] option:selected").value,
+          sauce: template.find("[name='sauce'] option:selected").value,
+          toppings: {
+            meats: meatToppings,
+            nonMeats: nonMeatToppings
+          },
+          custom: true,
+          price: 1000
+        };
+      }
+
+      if (pizza.name === "Pick a pizza!") {
+        Bert.alert("Make sure to pick a pizza!", "warning");
+      } else {
+        order.pizza = pizza._id ? pizza._id : customPizza;
+
+        Meteor.call('placeOrder', order, function (error, response) {
+          if (error) {
+            Bert.alert(error.reason, "danger");
+          } else {
+            Bert.alert("Order submitted!", "success");
+
+            if (order.credentials) {
+              Meteor.loginWithPassword(order.credentials.email, order.credentials.password);
+            }
+
+            FlowRouter.go('profile');
+          }
+        });
+      }
+    }
+  });
+});
+
 Template.order.helpers({
   customer: function () {
     if (Meteor.user()) {
