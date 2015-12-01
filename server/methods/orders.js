9708f561ee30d01accabb303e663914084354f0f
stevenjmarsh <sjm.atl@gmail.com>
2015-12-01 16:44:07 -0500

    submit order, doesnt save custom built pizza

---
+++ server/methods/orders.js
@@ -0,0 +1,51 @@
+Meteor.methods({
+  placeOrder: function (order) {
+    check(order, Object);
+
+    var handleOrder = {
+      createUser: function (credentials) {
+        try {
+          var userId = Accounts.createUser(credentials);
+          return userId;
+        } catch (exception) {
+          throw exception;
+        }
+      },
+
+      createCustomer: function (customer, userId) {
+        customer.userId = userId;
+        var customerId = Customers.insert(customer);
+
+        return customerId;
+      },
+
+      createPizza: function (pizza, userId) {
+        pizza.ownerId = userId;
+        var pizzaId = Pizza.insert(pizza);
+
+        return pizzaId;
+      },
+
+      createOrder: function (userId, pizzaId) {
+        var orderId = Orders.insert({
+          userId: userId,
+          pizzaId: pizzaId,
+          date: (new Date())
+        });
+
+        return orderId;
+      }
+    };
+
+    try {
+      var userId = order.credentials ? handleOrder.createUser(order.credentials) : order.customer,
+        customerId = order.customer.name ? handleOrder.createCustomer(order.customer, userId) : null,
+        pizzaId = order.pizza.custom ? handleOrder.createPizza(order.pizza, userId) : order.pizza,
+        orderId = handleOrder.createOrder(userId, pizzaId);
+
+      return orderId;
+    } catch (exception) {
+      throw exception;
+    }
+  }
+});
